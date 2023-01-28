150141b24da37c0771bd73b52b068435c3dfc865
Valentino <90069922+vffv2000@users.noreply.github.com>
2023-01-28 21:58:15 +0300

    Update models.py
    Закоментил все изменения которые кажутся логичными

--- school/models.py
+++ school/models.py
@@ -5,8 +5,9 @@ class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='прозвішча, імя')
     amount_courses = models.IntegerField(default=0, verbose_name='колькасць наведвальных курсаў')
     courses = models.ManyToManyField("Course",verbose_name="урок")
-    email = models.EmailField(verbose_name="пошта",blank=True)
-
+    mail=models.CharField(max_length=30, verbose_name="email адрес")# значчение email адрес уже подтягивается из формы
+    phone=models.CharField(max_length=30, verbose_name="номер телефона")# Так же было бы хорошо если подтягивали номер телефона
+    
     def __str__(self):
         return self.name
 
@@ -21,7 +22,6 @@ class Teacher(models.Model):
     education = models.CharField(max_length=20, blank=True,verbose_name='адукацыя')
     work_experience = models.IntegerField(blank=True, verbose_name='стаж')
     amount_courses = models.IntegerField(default=0, verbose_name='колькасць курсаў')
-    email = models.EmailField(verbose_name="пошта",blank=True)
 
     def __str__(self):
         return self.name
@@ -37,8 +37,10 @@ class Course(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT, verbose_name='настаўнік')
     time_lesson = models.TimeField(verbose_name='час правядзення')
     amount_studens = models.IntegerField(default=0, verbose_name='колькасць вучаняў зараз')
-    amount_places = models.IntegerField(default=5, verbose_name='ўсяго месц')
-    is_full = models.BooleanField(default=False, verbose_name='запоўнены')
+    is_full=models.BooleanField(default=False, verbose_name="Группа укомплектована")#С помощью этой строки будет легче выбирать группы в которые ещё требуются ученики по сравнению со стокой ниже. 
+    # Или если если потребуется уменьшить группы или наооборот
+    #amount_places = models.IntegerField(default=5, verbose_name='ўсяго месц')
+
 
     def __str__(self):
         return f"{self.subject} {self.grade} клас"
@@ -46,14 +48,3 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
-
-
-class Blog(models.Model):
-    title = models.CharField(max_length=100, verbose_name='назва')
-    content = models.TextField(blank=True, verbose_name='тэкст')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='створана')
-    update_at = models.DateTimeField(auto_now=True, verbose_name='зменены')
-    photo = models.ImageField(upload_to='site_pictures/%Y/%m/%d', verbose_name='фота', blank=True)
-    is_published = models.BooleanField(default=True, verbose_name='апублікавана')
-    # tag = models.ForeignKey('Tag', on_delete=models.PROTECT)
-
