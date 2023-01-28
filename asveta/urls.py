c6a9d70812062bff93c30399aa04fb88788f14cb
Kate Zinchenko <id.a1430@gmail.com>
2023-01-28 16:26:04 +0300

    second commit

--- asveta/urls.py
+++ asveta/urls.py
@@ -12,14 +12,12 @@
 
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from school.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/courselist/', CourseAPIList.as_view()),
-    path('api/v1/courselist/<int:pk>/', CourseAPIUpdate.as_view()),
-    path('api/v1/coursedetail/<int:pk>/', CourseAPIDetailView.as_view()),
+    path('', include('school.urls')),
 ]
--- school/urls.py
+++ school/urls.py
@@ -8,3 +8,12 @@
 #     path('info/', info, name='info'),
 #     path('recording/', recording, name='recording'),
 #]
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('courselist/', CourseAPIList.as_view()),
+    path('courselist/<int:pk>/', CourseAPIUpdate.as_view()),
+    path('coursedetail/<int:pk>/', CourseAPIDetailView.as_view()),
+]
