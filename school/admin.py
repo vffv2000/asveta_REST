13f92658a14d71bc974a95d903add40cdc8b1ba0
Valentino <90069922+vffv2000@users.noreply.github.com>
2023-01-29 16:14:09 +0300

    Update admin.py

--- school/admin.py
+++ school/admin.py
@@ -8,6 +8,7 @@ class Student_Admin(admin.ModelAdmin):
     list_display = ('pk', 'name','amount_courses')
     list_display_links = ('name',)
     search_fields = ('name',)
+    list_filter = ('amount_courses',)# создаёт фильтр по этим значениям. Которым можно пользоваться в админ панели на сайте 
 
 admin.site.register(Student, Student_Admin)
 
@@ -16,21 +17,26 @@ class Teacher_Admin(admin.ModelAdmin):
     list_display = ('pk', 'name','education','specialization','work_experience','amount_courses')
     list_display_links = ('name',)
     search_fields = ('name','specialization')
+    list_filter = ('specialization', 'work_experience','amount_courses')# создаёт фильтр по этим значениям. Которым можно пользоваться в админ панели на сайте 
 
 admin.site.register(Teacher, Teacher_Admin)
 
 class Course_Admin(admin.ModelAdmin):
-    list_display = ('pk', 'subject','grade','teacher','time_lesson')
+    list_display = ('pk', 'subject','grade','teacher','time_lesson','amount_studens','amount_places','is_full')# добавил поля для отображения 'amount_studens','amount_places','is_full'
     list_display_links = ('subject',)
     search_fields = ('subject','grade','teacher')
+    list_filter = ('subject', 'grade','is_full','amount_places')# создаёт фильтр по этим значениям. Которым можно пользоваться в админ панели на сайте 
+    list_editable = ('is_full',)# позволяет редактировать поля не заходя во внутрь
 
 admin.site.register(Course, Course_Admin)
 
 class Blog_Admin(admin.ModelAdmin):
-    list_display = ('pk', 'title','content','update_at','is_published','photo')
+    list_display = ('pk', 'title','content','created_at','update_at','is_published','photo')
     list_display_links = ('title',)
     search_fields = ('subject','grade','teacher')
-
+    list_editable = ('is_published',)# если это работает так как я думаю, то она должна статья будет убираться если убрать эту галочку и наоборот
+    list_filter = ('is_published', 'created_at')
+    
     def get_photo(self, obj):
         if obj.photo:
             return mark_safe(f'<img scr=" {obj.photo.url}" width=75>')
